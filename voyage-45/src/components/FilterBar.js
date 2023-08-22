6a749e336923fffed4a4a381e1981c8c1f73cc24
Denise Harker <zittingcodes@gmail.com>
2023-08-21 20:29:16 -0600

    Created the filter bar form and created some placement functions to call when clear/submit are clicked

--- voyage-45/src/components/FilterBar.js
+++ voyage-45/src/components/FilterBar.js
@@ -1,23 +1,61 @@
 import React, { useState, useEffect } from "react";
-import Image from 'next/image';
+import Image from "next/image";
+import styles from "./Filterbar.module.css";
 
 function FilterBar() {
-    const imgUrl = "https://img.icons8.com/?size=512&id=20006&format=png";
-    return (
-        <div style={{ display: "flex" }}>
-            <h1>FireBall</h1>
-            <form id='filter-bar-form'>
-                <input type="text" />
-                <button>Search</button>
-            </form>
+  const toggleButton = (e) => {
+    e.preventDefault();
+    alert("Button has been pressed");
+  };
+
+  const clear = (e) => {
+    e.preventDefault();
+    alert("Button has been pressed");
+  }
+  const imgUrl = "https://img.icons8.com/?size=512&id=20006&format=png";
+  return (
+    <div className={styles.formContainer}>
+      <form onSubmit={toggleButton} className={styles.form} id="filter-bar-form">
+        <button type='button' onClick={clear} className={styles.button}>Clear</button>
+        <input placeholder="Name" type="text" />
+        <div>
+          <label for="year-strike">Year</label>
+          <input type="number" min={1000} max={3000} id="year-strike" />
+        </div>
+        <div>
+          <label for="class-type">Class</label>
+          <input typ="text" id="class-type" />
+        </div>
+        <div className={styles.range}>
+            <label for="mass-range">Mass Range</label>
+          <div className={styles.checkbox}>
+            <span>Small</span>
+            <input type="checkbox" />
+          </div>
+          <div className={styles.checkbox}>
+            <span>Mid</span>
+            <input type="checkbox" />
+          </div>
+          <div className={styles.checkbox}>
+            <span>Large</span>
+            <input type="checkbox" />
+          </div>
+        </div>
+        <button type='submit' className={styles.button}>
+          Submit
+        </button>
+      </form>
+      {/* <div onClick={toggleButton} style={{cursor: 'pointer'}}>
+
             <Image
       src={imgUrl}
       width={32}
       height={32}
       alt="toggle icon"
     />
-        </div>
-    )
+      </div> */}
+    </div>
+  );
 }
 
 export default FilterBar;
