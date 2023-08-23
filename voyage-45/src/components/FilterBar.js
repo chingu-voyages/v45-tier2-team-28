f3dbb5c6304f2976c16fd4a841b5b48dd33291cf
Denise Harker <zittingcodes@gmail.com>
2023-08-23 15:34:09 -0600

    Created working filter bar where user can search name, year, type, and mass range. They can clear all filters if necessary, and the map updates appropriately.

--- voyage-45/src/components/FilterBar.js
+++ voyage-45/src/components/FilterBar.js
@@ -2,47 +2,44 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "./Filterbar.module.css";
 
-function FilterBar() {
-  const toggleButton = (e) => {
-    e.preventDefault();
-    alert("Button has been pressed");
-  };
-
-  const clear = (e) => {
-    e.preventDefault();
-    alert("Button has been pressed");
-  }
+function FilterBar(props) {
   const imgUrl = "https://img.icons8.com/?size=512&id=20006&format=png";
   return (
     <div className={styles.formContainer}>
-      <form onSubmit={toggleButton} className={styles.form} id="filter-bar-form">
-        <button type='button' onClick={clear} className={styles.button}>Clear</button>
-        <input placeholder="Name" type="text" />
+      <form
+        onSubmit={props.submit}
+        className={styles.form}
+        id="filter-bar-form"
+      >
+        <input onChange={props.updateName} placeholder="Name" type="text" value={props.name} />
         <div>
           <label for="year-strike">Year</label>
-          <input type="number" min={1000} max={3000} id="year-strike" />
+          <input onChange={props.updateYear} type="number" min={1000} max={3000} id="year-strike" value={props.year} />
         </div>
         <div>
           <label for="class-type">Class</label>
-          <input typ="text" id="class-type" />
+          <input onChange={props.updateType} typ="text" id="class-type" value={props.type} />
         </div>
         <div className={styles.range}>
-            <label for="mass-range">Mass Range</label>
+          <label for="mass-range">Mass Range</label>
           <div className={styles.checkbox}>
             <span>Small</span>
-            <input type="checkbox" />
+            <input onChange={props.updateSelected} name="small" checked={props.mass.small} type="radio" value="0-1000"/>
           </div>
           <div className={styles.checkbox}>
             <span>Mid</span>
-            <input type="checkbox" />
+            <input onChange={props.updateSelected} name="mid" checked={props.mass.mid} type="radio" value="1000-10000"/>
           </div>
           <div className={styles.checkbox}>
             <span>Large</span>
-            <input type="checkbox" />
+            <input onChange={props.updateSelected} name="large" checked={props.mass.large} type="radio" value="10000-1000000"/>
           </div>
         </div>
-        <button type='submit' className={styles.button}>
+        <button type="submit" className={styles.button}>
           Submit
+        </button>
+        <button type="button" onClick={props.clear} className={styles.button}>
+          Clear
         </button>
       </form>
       {/* <div onClick={toggleButton} style={{cursor: 'pointer'}}>
