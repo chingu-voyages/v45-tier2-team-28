862ce65c75424d45b98060106c32c62669f9a118
Lesley <tomosadalesley@gmail.com>
2023-09-07 21:13:29 -0700

    completed sm m l categorization

--- voyage-45/src/components/AverageMass.js
+++ voyage-45/src/components/AverageMass.js
@@ -1,27 +1,66 @@
 function AverageMass(props) {
-    let sum = 0
-    let count = 0
-    for (let meteor of props.data){
+
+    const calculateAvg = (list) => {
+    let totalSum = 0
+    let totalCount = 0
+    for (let meteor of list){
         const mass = meteor.properties?.mass
 
         if (mass !== undefined && mass != null){
-            sum += mass
-            count++
+            totalSum += mass
+            totalCount++
         }
+
     }
-    let avg = count === 0 ? 0: Math.round(sum/count)
+
+    let avg = totalCount === 0 ? 0: Math.round(totalSum/totalCount)
+    return avg
+  }
+  let smallList = props.data.filter((meteor) => {
+    const mass = meteor.properties.mass
+    return mass!== undefined && mass != null && mass < 1000
+  })
+
+  let medList = props.data.filter((meteor) => {
+    const mass = meteor.properties.mass
+    return mass!== undefined && mass != null && (mass < 10000 && 1000 <= mass)
+  })
+
+  let largeList = props.data.filter((meteor) => {
+    const mass = meteor.properties.mass
+    return mass!== undefined && mass != null && (mass >= 10000)
+  })  
+
+  let smallAvg = calculateAvg(smallList)
+  let medAvg = calculateAvg(medList)
+  let largeAvg = calculateAvg(largeList)
+  let totalAvg = calculateAvg(props.data)
 
     return (
       <div>
         <table>
           <thead>
             <tr>
+              <th>Category</th>
               <th>Average Mass</th>
             </tr>
           </thead>
           <tbody>
+          <tr>
+                <td>Small</td>
+                <td>{smallAvg.toString()}</td>
+              </tr>
               <tr>
-                <td>{avg.toString()}</td>
+                <td>Medium</td>
+                <td>{medAvg.toString()}</td>
+              </tr>
+              <tr>
+                <td>Large</td>
+                <td>{largeAvg.toString()}</td>
+              </tr>
+              <tr>
+                <td>Total</td>
+                <td>{totalAvg.toString()}</td>
               </tr>
           </tbody>
         </table>
