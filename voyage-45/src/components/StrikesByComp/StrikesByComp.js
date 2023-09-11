6595fe58ed8842953b92dfd5d9bf17cc314c30dd
Denise Zitting <zittingcodes@gmail.com>
2023-09-11 14:18:17 -0600

    Recreated graph into sideways bar graph

--- voyage-45/src/components/StrikesByComp/StrikesByComp.js
+++ voyage-45/src/components/StrikesByComp/StrikesByComp.js
@@ -1,54 +1,71 @@
-import React, { useEffect, useState } from 'react';
-import Chart from 'chart.js/auto'; // Import the Chart object properly
+import React, { useEffect, useRef } from 'react'; // Import useRef for the canvas element
+import Chart from 'chart.js/auto';
 
 function StrikesByComp(props) {
-  // Initialize the dataSet state
-  const [dataSet, setDataSet] = useState([]);
+  // Use useRef to get a reference to the canvas element
+  const chartRef = useRef();
 
   useEffect(() => {
     // Extract unique types using Set for the pie labels
-    const labels = [...new Set(props.data.map(item => item.properties.type))];
+    const labels = [...new Set(props.data.map((item) => item.properties.type))];
 
     // Calculate the count for each type
-    const typeCounts = labels.map(type => {
-      const count = props.data.filter(item => item.properties.type === type).length;
+    const typeCounts = labels.map((type) => {
+      const count = props.data.filter((item) => item.properties.type === type).length;
       return { name: type, count: count };
     });
 
-    // Update the dataSet state with the calculated counts
-    setDataSet(typeCounts);
-
-    const ctx = document.getElementById('strikeChart');
-
     // Generate an array of colors with varying red values
-    const backgroundColors = labels.map((_, index) => `rgb(${255 - (index * 5)}, 56, 0)`);
+    const backgroundColors = labels.map((_, index) => `rgba(${255 - index * 5}, 56, 0, 0.5)`);
 
     const data = {
-      labels: labels,
+      labels: labels, // Use "labels" for the label property
       datasets: [
         {
           label: 'Strikes by Composition',
-          data: typeCounts.map(item => item.count), // Use typeCounts for data
-          backgroundColor: backgroundColors, // Use the generated colors
+          data: typeCounts.map((item) => item.count),
+          backgroundColor: backgroundColors,
+          borderColor: '#8E7C77',
+          borderWidth: 3,
+          pointStyle: 'rect',
           hoverOffset: 15,
         },
       ],
     };
 
-    const chart = new Chart(ctx, {
-      type: 'pie',
-      data: data,
-    });
+    // Check if the chartRef is available before creating the chart
+    if (chartRef.current) {
+      const ctx = chartRef.current.getContext('2d');
+      const chart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: {
+          indexAxis: 'y',
+          animations: {
+            animateRotate: true, // Add rotation animation
+            animateScale: true, // Add scaling animation
+            duration: 2000,
+            easing: 'easeOutBack'
+          },
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
 
-    return () => {
-      // Destroy the Chart.js instance when the component unmounts or data alters
-      chart.destroy();
-    };
+      return () => {
+        // Destroy the Chart.js instance when the component unmounts or data alters
+        chart.destroy();
+      };
+    }
   }, [props.data]); // Run this effect whenever props.data changes
 
   return (
     <div>
-      <canvas id="strikeChart" style={{ maxWidth: 550, maxHeight: 550 }}></canvas>
+      {/* Use the chartRef for the canvas element */}
+      <canvas id="strikeChart" ref={chartRef} style={{ maxWidth: 550, maxHeight: 550 }}></canvas>
     </div>
   );
 }
